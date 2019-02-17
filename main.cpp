92da0cb4e5c6c4901ee6b7f786cd02879916912f
Mike Zamansky <zamansky@gmail.com>
2019-02-17 10:54:58 -0500

    added base files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+#include "funcs.h"
+
+int main()
+{
+  // Call the functions you wrote in funcs here
+  
+  return 0;
+}
